69ab990599212176e568b2db795065530c4bda95
mart-code <aamadewoyin@gmail.com>
2025-07-31 12:18:48 +0100

    sass-dashboard initial setups with simple layouts

---
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AccountToggle } from "./AccountToggle";
+
+export const Sidebar = () => {
+  return (
+    <div className="">
+      <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
+        <AccountToggle/>
+      </div>
+
+      {/* plan toggle */}
+    </div>
+  );
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-stone-100 text-stone-950`}
       >
         {children}
       </body>
